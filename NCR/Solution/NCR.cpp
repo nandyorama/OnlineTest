82b80c5288fd9a740e6052206ebd9eca4a37a12a
pramod <pramod.nandy06@gmail.com>
2016-11-01 22:50:35 +0530

    Update NCR.cpp
    Main file

--- NCR/Solution/NCR.cpp
+++ NCR/Solution/NCR.cpp
@@ -1,9 +1,4 @@
-// This is the main DLL file.
-
-#include "stdafx.h"
-
-#include "NCRLibrary.h"
-
+#include "NCR.h"
 int NCRSample::cmp(const void * a,const void * b)
 {
 	return *(char *)a - *(char *)b;
